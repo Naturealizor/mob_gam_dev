98b9dd05c356e8d2b81101885d57c974603904c3
Naturealizor <31334963+Naturealizor@users.noreply.github.com>
2019-10-16 20:02:47 -0500

    Added wall collision, working on death screen message and completing map

--- Assets/Scripts/Space_Ship_2D.cs
+++ Assets/Scripts/Space_Ship_2D.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class Space_Ship_2D : MonoBehaviour
 {
@@ -9,44 +11,36 @@ public class Space_Ship_2D : MonoBehaviour
     public float rotSpeed = 10;
     public int health = 100;
     public int score = 0;
+    TextMeshPro scoreText;
     
 
     private float turnDirection = 0;
     Rigidbody2D rb;
-    // public Button BtnLeft = null;
-    // public Button BtnRight = null;
     // Start is called before the first frame update
     void Start()
     {
-        // Button btn = BtnLeft.GetComponent<Button>();
-       // Button btn = BtnRight.GetComponent<Button>();
-        // btn.onClick.AddListener(TaskOnClick);
-
         rb = this.GetComponent<Rigidbody2D>();
         pauseMenu.SetActive(false);
+        deathScreen.SetActive(false);
         resetButton.SetActive(false);
         saveButton.SetActive(false);
+        scoreText = GameObject.Find("ScoreText").GetComponent<TextMeshPro>();
     }
 
     // Update is called once per frame
     void Update()
     { 
-        // if(Input.GetKey(KeyCode.RightArrow)) { 
-        //     turnDirection = -1;
-        // } else if(Input.GetKey(KeyCode.LeftArrow)) {  
-        //     turnDirection = 1;
-        // } else {
-        //     turnDirection = 0;
-        // }
+        if(Input.GetKey(KeyCode.RightArrow)) { 
+            turnDirection = -1;
+        } else if(Input.GetKey(KeyCode.LeftArrow)) {  
+            turnDirection = 1;
+        } else {
+            turnDirection = 0;
+        }
 
-        // if(Input.GetKeyDown(KeyCode.Escape)) {
-        //     Pause();
-        // }
-        // if(BtnLeft != null) {
-        //     BtnLeft.onClick.AddListener(GoLeft);
-        // } else if (BtnRight != null) {
-        //     BtnRight.onClick.AddListener(GoRight);
-        // }
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            Pause();
+        }
     }
 
     void FixedUpdate() 
@@ -58,22 +52,22 @@ public class Space_Ship_2D : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other) {
         if(other.gameObject.CompareTag("Asteroid")) {
             health -= 10;
-            if(health <= 0) {
-                print("You Died");
-                //Destroy(this.gameObject);
-            }
+            //Death();
         } else if(other.gameObject.CompareTag("Pickup")) {
             score += 50;
-            Destroy(other.gameObject);
+            scoreText.text = "Score = " + score;
+        } else if(other.gameObject.CompareTag("Wall")) {
+            health -= 100;
+            //Death();
         }
     }
 
     public bool gameIsPaused = false;
+    public bool playerIsDead = false;
     public GameObject pauseMenu;        // make sure you assign this or there will be errors!!!
     public GameObject resetButton;
     public GameObject saveButton;
-    // public GameObject BtnLeft;
-    // public GameObject BtnRight;
+    public GameObject deathScreen;
 
 
     void Pause() {
@@ -97,6 +91,18 @@ public class Space_Ship_2D : MonoBehaviour
         gameIsPaused = !gameIsPaused;
     }
 
+    void youDied() {
+        if(health = 0) {
+            playerIsDead = true;
+            if (playerIsDead) {
+                deathScreen.SetActive(true);
+                Destroy(this.gameObject);
+                SceneManager.LoadScene("Space_Collector");
+            }
+        }
+
+    }
+
     public void Save() {
         PlayerPrefs.SetInt("Health", health);
         PlayerPrefs.SetInt("Score", score);
@@ -117,15 +123,8 @@ public class Space_Ship_2D : MonoBehaviour
         turnDirection = direction;
     }
 
-    // public void TaskOnClick() {
-    //     Debug.Log("You've pressed the button!");
+    // public void Death() {
+        
     // }
 
-    // public void GoLeft() {
-    //     turnDirection = 1;
-    // }
-    
-    // public void GoRight() {
-    //     turnDirection = -1;
-    // }
 }
